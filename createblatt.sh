4acc8f7a02dff69ae6f06c82d889b336a1880b6f
camillo <camillo@90a692dc-14b0-426a-8af8-d4b69474ff9d>
2010-03-23 11:54:49 +0000

    createblatt erstellt aufgaben.tex auch selbständig

--- createblatt.sh
+++ createblatt.sh
@@ -18,5 +18,18 @@ cp -v BlattSkel/* "Blatt$BLATTNR"
 cd "Blatt$BLATTNR"
 sed -i "s/BLATTSPACE/Blatt $1/" blatt.tex
 sed -i "s/BLATTNOSPACE/blatt$1/" Makefile
+
+
+X=`expr \( $1 \* 3 \) - 2`
+Y=`expr \( $1 \* 3 \) - 1`
+Z=`expr \( $1 \* 3 \)`
+echo "\\section{Aufgabe $X}" > aufgabe$X.tex
+echo "\\section{Aufgabe $Y}" > aufgabe$Y.tex
+echo "\\section{Aufgabe $Z}" > aufgabe$Z.tex
+
+sed -i "s/AUFGABEX/aufgabe$X/" blatt.tex
+sed -i "s/AUFGABEY/aufgabe$Y/" blatt.tex
+sed -i "s/AUFGABEZ/aufgabe$Z/" blatt.tex
 mv -v blatt.tex "blatt$1.tex"
+
 echo "yuhu fertig, wen alles pass mit svn add Blatt$BLATTNR zum repository hinzufügen"
